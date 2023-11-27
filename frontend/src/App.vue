66a52ce33bf3b1ec194274ce45add7d0a9c6bf55
Diogo Faria <dluis34@gmail.com>
2023-11-27 12:08:04 +0000

    Started on frontend

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,30 +1,99 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
-</template>
+  <head>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+      <title>Home - Brand</title>
+      <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+      <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic&amp;display=swap">
+      <link rel="stylesheet" href="assets/fonts/fontawesome-all.min.css">
+  </head>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+  <body>
+    
+      <section style="background: url(&quot;assets/img/bg-masthead.jpg&quot;);margin-left: 0;margin-right: 0;">
+          <div class="container" style="padding: 5%;padding-bottom: 5%;padding-top: 5%;padding-right: 5%;padding-left: 5%;">
+              <div class="row">
+                  <div class="col-md-12 text-center align-self-center">
+                      <p class="fs-5 fw-bolder" style="margin-bottom: 1%;margin-top: 1%;color: #1C7330;"><strong><span style="color: rgb(28, 115, 48);">[WHAT</span></strong>&nbsp;WE DO]<br></p>
+                  </div>
+              </div>
+              <div class="row">
+                  <div class="col-xl-1"></div>
+                  <div class="col-md-6 col-xl-7 text-center align-self-center"><input class="form-control-lg" type="search" placeholder="Search for a Company..." style="width: 100%;border-radius: 15px;border: 2px solid #1C7330 ;"></div>
+                  <div class="col-md-6 col-xl-2 text-center align-self-center"><button class="btn btn-primary btn-lg" type="submit" style="background: #1C7330;">Search</button></div>
+                  <div class="col"></div>
+              </div>
+          </div>
+      </section>
+      <section style="margin-top: 3%;margin-bottom: 3%;">
+          <div class="container">
+              <div class="row">
+                  <div class="col-md-12">
+                      <p class="fs-5 fw-bolder text-center" style="margin-bottom: 1%;margin-top: 1%;color: #1C7330;">Top Predictions</p>
+                  </div>
+              </div>
+              <div class="row">
+                  <div class="col">
+                      <ul class="list-group">
+                          <li class="list-group-item" style="--bs-body-bg: #959595;background: rgb(241,241,241);">
+                              <div class="row">
+                                  <div class="col-md-3 col-xl-1 text-center align-self-center"><i class="fas fa-caret-up fs-1" style="color: #1C7330;"></i></div>
+                                  <div class="col-md-3 text-center align-self-center"><span>Company Name</span></div>
+                                  <div class="col-md-3 col-xl-7 text-center align-self-center"><span>Prediction Info</span></div>
+                                  <div class="col-md-3 col-xl-1 text-center align-self-center"><i class="fas fa-angle-down fs-3" style="color: #1C7335;"></i></div>
+                              </div>
+                          </li>
+                          <li class="list-group-item" style="--bs-body-bg: #959595;background: rgb(241,241,241);">
+                              <div class="row">
+                                  <div class="col-md-3 col-xl-1 text-center align-self-center"><i class="fas fa-caret-down fs-1" style="color: #9c0900;"></i></div>
+                                  <div class="col-md-3 text-center align-self-center"><span>Company Name</span></div>
+                                  <div class="col-md-3 col-xl-7 text-center align-self-center"><span>Prediction Info</span></div>
+                                  <div class="col-md-3 col-xl-1 text-center align-self-center"><i class="fas fa-angle-down fs-3" style="color: #1C7335;"></i></div>
+                              </div>
+                          </li>
+                          <li class="list-group-item" style="--bs-body-bg: #959595;background: rgb(241,241,241);">
+                              <div class="row">
+                                  <div class="col-md-3 col-xl-1 text-center align-self-center"><i class="fas fa-caret-up fs-1" style="color: #1C7330;"></i></div>
+                                  <div class="col-md-3 text-center align-self-center"><span>Company Name</span></div>
+                                  <div class="col-md-3 col-xl-7 text-center align-self-center"><span>Prediction Info</span></div>
+                                  <div class="col-md-3 col-xl-1 text-center align-self-center"><i class="fas fa-angle-down fs-3" style="color: #1C7335;"></i></div>
+                              </div>
+                          </li>
+                          <li class="list-group-item" style="--bs-body-bg: #959595;background: rgb(241,241,241);">
+                              <div class="row">
+                                  <div class="col-md-3 col-xl-1 text-center align-self-center"><i class="fas fa-caret-down fs-1" style="color: #9c0900;"></i></div>
+                                  <div class="col-md-3 text-center align-self-center"><span>Company Name</span></div>
+                                  <div class="col-md-3 col-xl-7 text-center align-self-center"><span>Prediction Info</span></div>
+                                  <div class="col-md-3 col-xl-1 text-center align-self-center"><i class="fas fa-angle-down fs-3" style="color: #1C7335;"></i></div>
+                              </div>
+                          </li>
+                          <li class="list-group-item text-center" style="--bs-body-bg: #959595;background: rgb(241,241,241);"><a href="#" style="color: #1C7330;">Click to See More</a></li>
+                      </ul>
+                  </div>
+              </div>
+          </div>
+      </section>
+      <footer class="bg-light footer">
+          <div class="container">
+              <div class="row">
+                  <div class="col-lg-6 text-center text-lg-start my-auto h-100">
+                      <ul class="list-inline mb-2">
+                          <li class="list-inline-item"><a href="#">About</a></li>
+                          <li class="list-inline-item"><span style="color: var(--bs-black);font-size: 16;font-weight: bold;">⋅ </span></li>
+                          <li class="list-inline-item"><a href="#">Contact</a></li>
+                          <li class="list-inline-item"><span style="font-size: 16px;font-weight: bold;">⋅</span></li>
+                          <li class="list-inline-item"><a href="#">Terms of &nbsp;Use</a></li>
+                          <li class="list-inline-item"><span style="font-weight: bold;">⋅</span></li>
+                          <li class="list-inline-item"><a href="#">Privacy Policy</a></li>
+                      </ul>
+                      <p class="text-muted small mb-4 mb-lg-0">© Money Talks 2023. All Rights Reserved.</p>
+                  </div>
+                  <div class="col-lg-6 text-center text-lg-end my-auto h-100"></div>
+              </div>
+          </div>
+      </footer>
+  </body>
+</template>
---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+  <nav class="navbar navbar-light navbar-expand bg-light navigation-clean">
+    <div class="container"><a class="navbar-brand fs-4 fw-bold" href="#" style="--bs-body-color: #1C7330;color: #1C7330;">&nbsp;Money Talks</a><button data-bs-toggle="collapse" class="navbar-toggler" data-bs-target="#navcol-1"></button>
+        <div class="collapse navbar-collapse" id="navcol-1"></div>
+    </div>
+   </nav>
+</template>
+
+<script lang="ts">
+  import { defineComponent } from 'vue';
+
+  export default defineComponent({
+    name: 'Header',
+  });
+</script>
