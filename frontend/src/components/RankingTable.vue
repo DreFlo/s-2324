0ca41a2817447a65a35403ac7f73d5ece54a2e0c
Diogo Faria <dluis34@gmail.com>
2023-11-30 16:12:39 +0000

    Added ranking page

--- frontend/src/components/RankingTable.vue
+++ frontend/src/components/RankingTable.vue
@@ -27,13 +27,19 @@
                                     {{ ranking['score'] }}
                                 </span>
                             </div>
-                            <div class="col-1 text-center align-self-center">
-                                <i class="bi bi-chevron-down" style="color: #1C7335;"></i>
+                            <div id="bttn{{ ranking['newRanking'] }}" class="col-1 text-center align-self-center">
+                                <i class="bi bi-chevron-down" data-bs-toggle="collapse" :href="'#collapse'+ ranking['newRanking'] " role="button" aria-expanded="false" :aria-controls="'collapse'+ ranking['newRanking']" style="color: #1C7335;"></i>
+                            </div>
+                        </div>
+                        <div class="collapse" :id="'collapse'+ ranking['newRanking']">
+                            <div class="card card-body">
+                                Prediction Info
                             </div>
                         </div>
                     </li>
-                    <li class="list-group-item text-center" style="--bs-body-bg: #959595;background: rgb(241,241,241);">
-                        <a href="#" style="color: #1C7330;">
+                    
+                    <li v-if="this.numRankings != null" class="list-group-item text-center" style="--bs-body-bg: #959595;background: rgb(241,241,241);">
+                        <a @click="goToRankings" style="color: #1C7330; cursor:pointer">
                             Click to See More
                         </a>
                     </li>
@@ -53,6 +59,7 @@ export default defineComponent({
         numRankings: {
             type: Number,
             required: false,
+            default: null
         },
     },
     data() {
@@ -67,12 +74,23 @@ export default defineComponent({
         getRankings() {
             //add axios call to get rankings
             
-            this.rankings.push({companyId: 1, companyName: 'Company 1', score: 0.9, oldRanking: 1, newRanking: 1});
-            this.rankings.push({companyId: 2, companyName: 'Company 2', score: 0.8, oldRanking: 2, newRanking: 2});
-            this.rankings.push({companyId: 3, companyName: 'Company 3', score: 0.7, oldRanking: 4, newRanking: 3});
-            this.rankings.push({companyId: 4, companyName: 'Company 4', score: 0.6, oldRanking: 3, newRanking: 4});
-            this.rankings.push({companyId: 4, companyName: 'Company 4', score: 0.5, oldRanking: 5, newRanking: 5});
+            this.rankings.push({companyId: 1, companyName: 'Company 1', score: 0.90, oldRanking: 1, newRanking: 1});
+            this.rankings.push({companyId: 2, companyName: 'Company 2', score: 0.88, oldRanking: 2, newRanking: 2});
+            this.rankings.push({companyId: 3, companyName: 'Company 3', score: 0.86, oldRanking: 4, newRanking: 3});
+            this.rankings.push({companyId: 4, companyName: 'Company 4', score: 0.84, oldRanking: 3, newRanking: 4});
+            this.rankings.push({companyId: 5, companyName: 'Company 4', score: 0.83, oldRanking: 5, newRanking: 5});
             
+            if (this.numRankings == null) {
+                this.rankings.push({companyId: 6, companyName: 'Company 6', score: 0.80, oldRanking: 8, newRanking: 6});
+                this.rankings.push({companyId: 7, companyName: 'Company 7', score: 0.78, oldRanking: 7, newRanking: 7});
+                this.rankings.push({companyId: 8, companyName: 'Company 8', score: 0.77, oldRanking: 6, newRanking: 8});
+                this.rankings.push({companyId: 9, companyName: 'Company 9', score: 0.75, oldRanking: 9, newRanking: 9});
+                this.rankings.push({companyId: 10, companyName: 'Company 10', score: 0.74, oldRanking: 10, newRanking: 10});
+
+            }
+        },
+        goToRankings() {
+            this.$router.push({ name: 'rankings'});
         }
     }
 });
